5a75bcb268549c44b5480adf89b54f5525b3c21f
Pau Matas <pmatasalbiol@gmail.com>
2024-02-04 19:45:57 +0100

    feat: authentication modals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,13 +12,16 @@ export const metadata = {
 
 const inter = Inter({ subsets: ["latin"] }); // TODO: Fer servir la font de l'AED
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children, authModal }: { children: React.ReactNode; authModal: React.ReactNode }) {
 	return (
 		<html
 			lang="en"
 			className={cn("bg-white text-slate-900 antialiased light", inter.className)}>
 			<body className="min-h-screen pt-12 bg-slate-50 antialiased">
+				{/* @ts-expect-error server component */}
 				<Navbar />
+
+				{authModal}
 
 				<div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
 
