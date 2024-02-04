15c455b5c19c0920f87bd73dd472f6f2bd8252b0
Pau Matas <pmatasalbiol@gmail.com>
2024-02-04 18:45:51 +0100

    feat: user menu in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,13 @@ import { Icons } from "@/components/Icons";
 import Image from "next/image";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
 	const session = await getAuthSession();
 
 	return (
-		<div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+		<div className="fixed top-0 inset-x-0 h-auto bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
 			<div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
 				<Link
 					href="/"
@@ -26,8 +27,8 @@ const Navbar = async () => {
 
 				{/* search bar */}
 
-				{session ? (
-					<p>You are logged in</p>
+				{session && session.user ? (
+					<UserAccountNav user={session.user} />
 				) : (
 					<Link
 						href="/sign-in"
---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { User } from "next-auth";
+import { FC } from "react";
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+} from "@/components/ui/DropdownMenu";
+import UserAvatar from "@/components/UserAvatar";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+
+interface UserAccountNavProps {
+	user: Pick<User, "name" | "image" | "email">;
+}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<UserAvatar
+					user={{
+						name: user.name || null,
+						image: user.image || null,
+					}}
+					className="h-8 w-8"
+				/>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent
+				className="bg-white"
+				align="end">
+				<div className="flex items-center justify-start gap-2 p-2">
+					<div className="flex flex-col space-y-1 leading-none">
+						{user.name && <p className="font-medium">{user.name}</p>}
+						{user.email && <p className="w-[200px] truncate text-sm text-zinc-700">{user.email}</p>}
+					</div>
+				</div>
+
+				<DropdownMenuSeparator />
+
+				<DropdownMenuItem asChild>
+					<Link href="/">Inici</Link>
+				</DropdownMenuItem>
+
+				<DropdownMenuItem asChild>
+					<Link href="/settings">Preferències</Link>
+				</DropdownMenuItem>
+
+				<DropdownMenuSeparator />
+
+				<DropdownMenuItem
+					onSelect={(event) => {
+						event.preventDefault();
+						signOut({ callbackUrl: `${window.location.origin}/sign-in` });
+					}}
+					className="cursor-pointer">
+					Tancar Sessió
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+export default UserAccountNav;
