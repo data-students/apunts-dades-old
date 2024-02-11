a7ee82c8b64f011eeddfa62722ddb26fde15b97d
Pau Matas <pmatasalbiol@gmail.com>
2024-02-11 17:32:22 +0100

    home page rebuilt

--- src/app/[slug]/q/[questionId]/page.tsx
+++ src/app/[slug]/q/[questionId]/page.tsx
@@ -1,4 +1,4 @@
-import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import MiniCreateQuestion from "@/components/MiniCreateQuestion";
@@ -7,36 +7,36 @@ import { notFound } from "next/navigation";
 import { AnswersView } from "@/components/AnswersView";
 
 interface PageProps {
-    params: {
-        slug: string;
-    };
+	params: {
+		slug: string;
+	};
 }
 
 const page = async ({ params }: PageProps) => {
-    const { slug } = params;
-    // const session = await getAuthSession();
-    const subject = await db.subject.findFirst({
-        where: { acronym: slug },
-        include: {
-            questions: {
-                include: {
-                    author: true,
-                    votes: true,
-                    subject: true,
-                    answers: true,
-                },
-                take: INFINITE_SCROLLING_PAGINATION_RESULTS,
-            },
-        },
-    });
+	const { slug } = params;
+	// const session = await getAuthSession();
+	const subject = await db.subject.findFirst({
+		where: { acronym: slug },
+		include: {
+			questions: {
+				include: {
+					author: true,
+					votes: true,
+					subject: true,
+					answers: true,
+				},
+				take: INFINITE_SCROLL_PAGINATION_RESULTS,
+			},
+		},
+	});
 
-    if (!subject) return notFound();
+	if (!subject) return notFound();
 
-    return (
-        <div>
-            <AnswersView questionId = {"123"}/>
-        </div>
-    );
+	return (
+		<div>
+			<AnswersView questionId={"123"} />
+		</div>
+	);
 };
 
 export default page;
