ae3461946682daf45aedc29a4fb85dd48c50b2f8
Marc Franquesa Monés <marc.franquesam@gmail.com>
2024-03-11 16:51:22 +0100

    Canviar text box per boto

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { Session } from "next-auth"
-import { usePathname, useRouter } from "next/navigation"
-import { FC } from "react"
-import UserAvatar from "./UserAvatar"
-import { Input } from "./ui/Input"
+import { Session } from "next-auth";
+import { usePathname, useRouter } from "next/navigation";
+import { FC } from "react";
+import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/Button";
 
 interface MiniCreatePostProps {
   session: Session | null
@@ -14,25 +15,37 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
   const router = useRouter()
   const pathname = usePathname()
 
-  return (
-    <div className="overflow-hidden rounded-md bg-white shadow">
-      <div className="h-full px-6 py-4 flex justify-between gap-6">
-        <div className="relative">
-          <UserAvatar
-            user={{
-              name: session?.user?.name || null,
-              image: session?.user?.image || null,
-            }}
-          />
+	return (
+		<div className="overflow-hidden rounded-md bg-white shadow">
+			<div className="h-full px-6 py-6 flex justify-between gap-6">
+				<div className="relative">
+					<UserAvatar
+						user={{
+							name: session?.user?.name || null,
+							image: session?.user?.image || null,
+						}}
+					/>
+					{/* <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" /> */}
 
-          <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
-        </div>
+				</div>
+                
+                <Link
+                    href={pathname + "/submit"}
+                    className={buttonVariants({
+                        variant: "outline",
+                        className: "w-full",
+                    })}>
+                    Penja Apunts
+                </Link>
 
-        <Input
-          readOnly
-          onClick={() => router.push(pathname + "/submit")}
-          placeholder="Comparteix els teus apunts"
-        />
+                <Link
+                    href={pathname + "/q"}
+                    className={buttonVariants({
+                        variant: "outline",
+                        className: "w-full",
+                    })}>
+                    Llança una pregunta
+                </Link>
 
         {/* <Button
 					onClick={() => router.push(parentPathname + "/submit")}
