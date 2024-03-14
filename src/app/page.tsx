d11e47525dedfff37a0ca9cd10687cc9976da30d
Pau Matas <pmatasalbiol@gmail.com>
2024-03-15 00:43:02 +0100

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// import CustomFeed from "@/components/CustomFeed";
 import { buttonVariants } from "@/components/ui/Button"
 import { getAuthSession } from "@/lib/auth"
 import { FileTextIcon, HelpCircleIcon, HomeIcon, BookIcon } from "lucide-react"
@@ -90,35 +89,6 @@ export default async function Home() {
     <>
       <h1 className="font-bold text-3xl md:text-4xl">El teu espai</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-        {/* Feed
-				{session ? <CustomFeed /> : null} */}
-
-        {/* subjects info */}
-        {/* <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last mb-4">
-					<div className="bg-emerald-100 px-6 py-4">
-						<p className="font-semibold py-3 flex items-center gap-1.5">
-							<HomeIcon className="w-4 h-4" />
-							Home
-						</p>
-					</div>
-
-					<div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
-						<div className="flex justify-between gap-x-4 py-3">
-							<p className="text-zinc-500">
-								La teva pàgina d Apunts de Dades. Accedeix aquí per a veure els apunts de les assignatures que
-								t interessen.
-							</p>
-						</div>
-
-						<Link
-							className={buttonVariants({
-								className: "w-full mt-4 mb-6",
-							})}
-							href="/create">
-							Crea una assignatura
-						</Link>
-					</div>
-				</div> */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first mb-4">
           <div className="bg-pink-100 px-6 py-4">
             <p className="font-semibold py-3 flex items-center gap-1.5">
