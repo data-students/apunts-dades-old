ab018ca8e2700886ab6d4ed396b23d9aa34f157a
Pau Matas <pmatasalbiol@gmail.com>
2024-03-25 19:19:54 +0100

    add: new column in Post with correct values

---
+++ scripts/post-urls-from-content.js
@@ -0,0 +1,26 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function updateRecords() {
+  try {
+    const recordsToUpdate = await prisma.Post.findMany();
+
+    for (const record of recordsToUpdate) {
+      await prisma.Post.update({
+        where: { id: record.id },
+        data: {
+          urls: JSON.stringify([ record.content ]),
+        },
+      });
+    }
+
+    console.log('Records updated successfully.');
+  } catch (error) {
+    console.error('Error updating records:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+updateRecords();
